7343bc3f3daeccccc7f1f37c67d4fcd325e712db
Hoang Quoc Ly <hoanglyquoc@gmail.com>
2025-09-25 10:10:49 -0400

    connect db

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "bookshelf",
+    password: "123456",
+    port: 5432,     
+});
+db.connect()
+
 app.get('/', (req, res) => {
     res.render("index.ejs");
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
