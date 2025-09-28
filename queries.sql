acf7880b02aaadcca74c041ddda18a133454ab18
Hoang Quoc Ly <hoanglyquoc@gmail.com>
2025-09-27 20:39:47 -0400

    insert book query

--- queries.sql
+++ queries.sql
@@ -6,3 +6,8 @@ CREATE TABLE books(
     read_date date,
     note  TEXT
 );
+
+INSERT INTO books (id, title, author, isbn, read_date, note) VALUES
+(1, 'Atomic Habits', 'James Clear', '9780735211292', '2024-08-01',
+ 'A practical, psychology-driven guide to how small habits compound into big results. Clear breaks down the “habit loop” (cue, craving, response, reward) and shows how to design environments that make good habits easy and bad habits harder. His ideas on habit stacking and identity-based change are especially memorable — focus less on goals, more on systems and becoming the type of person you want to be.'
+);
